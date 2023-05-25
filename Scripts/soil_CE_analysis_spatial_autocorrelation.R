67794108f54de2efa8efc7524911deab13abd2a9
Bartosz Bartkowski <44167920+BartoszBartk@users.noreply.github.com>
2023-05-25 11:20:16 +0200

    autocorrelation analysis with jittered spatial points

--- Scripts/soil_CE_analysis_spatial_autocorrelation.R
+++ Scripts/soil_CE_analysis_spatial_autocorrelation.R
@@ -7,9 +7,13 @@
 
 ############## Outline ###############
 # 1. Packages and data
-# 2. Combine with spatial information
+# 2. Calculate WTP means for PLZ areas
 # 3. Spatial weighting matrices
 # 4. Calculate Moran's I
+# 5. Robustness check
+#  5.1. Jitter coordinates
+#  5.2. Recalculate spatial weighting matrix
+#  5.3. Recalculate Moran's I
 ######################################
 
 ######################
@@ -24,7 +28,7 @@ require(here)
 ce_data <- read.csv(here("Spatial data/ce_results_spatial_data_clean.csv"), header = T)
 
 ######################
-# 2. Combine with spatial information
+# 2. Calculate WTP means for PLZ areas
 ######################
 
 #calculate mean WTP scores for each PLZ (postcode)
@@ -111,3 +115,71 @@ moran.test(wtp_plz$flood, weights_knn)
 moran.test(wtp_plz$climate, weights_knn)
 moran.test(wtp_plz$water, weights_knn)
 
+######################
+# 5. Robustness check
+######################
+
+######################
+# 5.1. Jitter coordinates
+######################
+
+#read in data again
+ce_data <- read.csv(here("Spatial data/ce_results_spatial_data_clean.csv"),header = T)
+
+#identify duplicates (i.e. observations in same PLZ area)
+duplicates <- which(duplicated(ce_data$plz))
+
+#check the range of coordinates
+summary(ce_data$xcoor)
+summary(ce_data$ycoor)
+
+#manually jitter the coordinates of the duplicates (manually because I can't find a good alternative to the spJitter function from the discontinued spsann package)
+set.seed(2104)
+for (i in 1:length(duplicates)) {
+  ce_data$xcoor[duplicates[i]] <- ce_data$xcoor[duplicates[i]] + runif(1,-20,20)
+  ce_data$ycoor[duplicates[i]] <- ce_data$ycoor[duplicates[i]] + runif(1,-20,20)
+}
+#check results (if everything worked well, there shouldn't be any observations left that have the same coordinates)
+which(duplicated(ce_data$xcoor) & duplicated(ce_data$ycoor))
+
+#save modified data
+write.csv(ce_data, here("Spatial data/ce_results_spatial_data_jittered.csv"), fileEncoding = "UTF-8")
+
+######################
+# 5.2. Recalculate weighting matrix
+######################
+
+#extract coordinates
+coords <- ce_data[,c("xcoor","ycoor")]
+
+#full inverse-distance weighting
+dists <- as.matrix(dist(coords)) #make matrix of distances
+dists_i <- 1/dists #invert
+diag(dists_i) <- 0 #add diagonal
+row_sums <- apply(dists_i,1,sum) #calculate row sums
+dists_i <- dists_i/row_sums #normalize values using row sums
+weights <- mat2listw(dists_i,style="W") #reformat to have format readable by spdep
+
+#inverse-distance weighting for 41494 m range (in line with default settings in ArcGIS)
+dists_40 <- 1/dists
+diag(dists_40) <- 0
+dists_40[dists_40<1/41494] <- 0
+row_sums <- apply(dists_40,1,sum)
+dists_40 <- dists_40/row_sums
+weights_40 <- mat2listw(dists_40,style="W")
+
+######################
+# 5.3. Recalculate Moran's I
+######################
+
+#spatial autocorrelation with jittered coordinates and full inverse-distance weighting
+moran.test(ce_data$drought, weights)
+moran.test(ce_data$flood, weights)
+moran.test(ce_data$climate, weights)
+moran.test(ce_data$water, weights)
+
+#spatial autocorrelation with jittered coordinates and "ArcGIS weighting"
+moran.test(ce_data$drought, weights_40)
+moran.test(ce_data$flood, weights_40)
+moran.test(ce_data$climate, weights_40)
+moran.test(ce_data$water, weights_40)
