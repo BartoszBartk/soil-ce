d969e8ab3740941e2c6e850a3bffc7a0e551f84b
Bartosz Bartkowski <44167920+BartoszBartk@users.noreply.github.com>
2024-12-06 16:20:21 +0100

    some corrections, headings

--- BonaRes_test_case/test_case_economic_valuation.R
+++ BonaRes_test_case/test_case_economic_valuation.R
@@ -22,6 +22,10 @@ require(dplyr)
 require(reshape2)
 require(ggplot2)
 
+##########
+# 1. BODIUM data
+##########
+
 ## Read first chunk of BODIUM data
 bodium_data <- read.csv(here("Bodium/notill_diverse_mineral_26_sum.csv"), header = T, sep = "")
 
@@ -55,16 +59,15 @@ bodium_data <- rbind(bodium_data,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12)
 rm(d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12)
 
 # remove irrelevant columns
-bodium_data <- subset(bodium_data, select = c("Scenario", "Year", "Plant", "Plant_biomass_SO", "Nitrate_leach", "Corg", "AWC"))
+bodium_data <- subset(bodium_data, select = c("Scenario", "Year", "Plant", "Plant_biomass_SO", "Nitrate_leach", "Corg", "AWC", "Weight"))
 
 # restrict dataset to projection part
 bodium_dataset <- subset(bodium_data, Year > 2020)
 
 # recalculate plant biomass values to have them per ha and in tons
 bodium_dataset$Plant_biomass_SO <- bodium_dataset$Plant_biomass_SO * 10000 / 1000
-############# TEMPORARY SOLUTION BASED ON ROUGH ESTIMATE OF 1000 kg dry soil / m3
-# recalculate Corg from kg/kg to kg C / ha
-bodium_dataset$Corg <- bodium_dataset$Corg * 20000000
+# recalculate Corg from kg/kg to kg C / ha based on weight variable
+bodium_dataset$Corg <- bodium_dataset$Corg * 10000 * bodium_dataset$Weight
 
 # add columns soil management (conventional vs. conservation), crop rotation (simple vs. diverse) and fertilization (mineral vs. mixed)
 bodium_dataset <- bodium_dataset %>%
@@ -73,11 +76,19 @@ bodium_dataset <- bodium_dataset %>%
          Fertlzr = ifelse(grepl("mineral", Scenario), "mineral", "mixed")
   )
 
+##########
+# 2. WTP data
+##########
+
 ## Read WTP data (recalculated for relevant units)
 wtp_data <- read.csv("WTPs_recalculated.csv", header = T, sep = ";")
 
 ## Read price/cost data for food provision service
 price_data <- read.csv("costs_prices_food.csv", header = T, sep = ";", dec = ",")
+
+##########
+# 3. Merge datasets
+##########
 
 ## Add gross margins based on price_data 
 full_dataset <- merge(bodium_dataset, price_data, by.x = c("Plant","Soil_mngmt"), by.y = c("Crop","Soil_mngmt"))
@@ -88,22 +99,18 @@ full_dataset$GM <- full_dataset$Plant_biomass_SO * full_dataset$Price_ton - full
 ## Add WTP estimates
 full_dataset$WTP_Corg <- full_dataset$Corg * as.numeric(wtp_data[wtp_data$ES == "climate",]$WTP_per_unit)
 full_dataset$WTP_AWC <- full_dataset$AWC * as.numeric(wtp_data[wtp_data$ES == "drought",]$WTP_per_unit)
-full_dataset$WTP_nitrate <- full_dataset$Nitrate_leach * as.numeric(wtp_data[wtp_data$ES == "water",]$WTP_per_unit)
+full_dataset$WTP_nitrate <- full_dataset$Nitrate_leach * as.numeric(wtp_data[wtp_data$ES == "water",]$WTP_per_unit) * -1
 
 ## Calculate total value
 full_dataset$tot_wtp <- full_dataset$WTP_AWC + full_dataset$WTP_Corg + full_dataset$WTP_nitrate
 
+##########
+# 4. Yearly economic values without discounting
+##########
+
 ## Add total values per year to new scenario dataframe
 # simplify full_dataset
 temp_data <- subset(full_dataset, select = c("Scenario", "Year", "GM", "tot_wtp"))
-
-##################################
-# optional: discount the values
-r <- 0.04 # set discount rate for GM (private good)
-p <- 0.01 # set discount rate to tot_wtp (public goods)
-temp_data$GM <- temp_data$GM/(1 + r)^(temp_data$Year - min(temp_data$Year))
-temp_data$tot_wtp <- temp_data$tot_wtp/(1 + p)^(temp_data$Year - min(temp_data$Year))
-##################################
 
 # reshape to long format to have all monetary values in one column
 temp_data_ <- melt(temp_data,
@@ -123,8 +130,7 @@ val_data_ <- reshape(temp_data_, direction = "wide",
 colnames(val_data_) <- c("Scenario", c(2021:2049))
 val_data_ <- val_data_[order(val_data_$Scenario),]
 
-# add (discounted) sum for each scenario (for comparability, reduce to 24 data points, as crop rotation length is 6 in "diverse")
-val_data_$NPV <- rowSums(val_data_[, c(2:25)])
+# add moments for each scenario (for comparability, reduce to 24 data points, as crop rotation length is 6 in "diverse")
 val_data_$mean <- rowMeans(val_data_[, c(2:25)])
 val_data_$sd <- apply(val_data_[, c(2:25)], 1, sd)
 
@@ -140,20 +146,52 @@ val_data <- reshape(temp_data, direction = "wide",
                     v.names = "tot_val")
 colnames(val_data) <- c("Scenario", c(2021:2049))
 val_data <- val_data[order(val_data$Scenario),]
-# add (discounted) sum for each scenario (24 years only, see explanation above)
-val_data$NPV <- rowSums(val_data[, c(2:25)])
+# add moments for each scenario (24 years only, see explanation above)
 val_data$mean <- rowMeans(val_data[, c(2:25)])
 val_data$sd <- apply(val_data[, c(2:25)], 1, sd)
 
-## Create plots
+##########
+# 5. Yearly economic values with discounting
+##########
+
+## Repeat analogously to above
+temp_data2 <- subset(full_dataset, select = c("Scenario", "Year", "GM", "tot_wtp"))
+
+# discount the values
+r <- 0.04 # set discount rate for GM (private good)
+p <- 0.01 # set discount rate to tot_wtp (public goods)
+temp_data2$GM <- temp_data2$GM/(1 + r)^(temp_data2$Year - min(temp_data2$Year))
+temp_data2$tot_wtp <- temp_data2$tot_wtp/(1 + p)^(temp_data2$Year - min(temp_data2$Year))
+
+# repeat all calculations
+temp_data2$tot_val <- temp_data2$GM + temp_data2$tot_wtp
+temp_data2 <- temp_data2 %>% select(-c(GM, tot_wtp))
+temp_data2 <- temp_data2[order(temp_data2$Year),]
+val_data2 <- reshape(temp_data2, direction = "wide",
+                    idvar = "Scenario",
+                    timevar = "Year",
+                    v.names = "tot_val")
+colnames(val_data2) <- c("Scenario", c(2021:2049))
+val_data2 <- val_data2[order(val_data2$Scenario),]
+val_data2$NPV <- rowSums(val_data2[, c(2:25)])
+val_data2$mean <- rowMeans(val_data2[, c(2:25)])
+val_data2$sd <- apply(val_data2[, c(2:25)], 1, sd)
+
+##########
+# 7. Plotting
+##########
+
+## Create plots without discounting
 # create plots for each scenario
 # sums
 boxplots <- ggplot(temp_data, aes(x = Scenario, y = tot_val)) + 
-  geom_boxplot()
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 boxplots
 # separate GM / tot_wtp
 boxplots_ <- ggplot(temp_data_, aes(x = Scenario, y = value)) + 
-  geom_boxplot()
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 boxplots_
 # sums
 timeline <- ggplot(temp_data, aes(x = Year, y = tot_val, color = Scenario)) +
@@ -164,7 +202,8 @@ timeline_ <- ggplot(temp_data_, aes(x = Year, y = value, color = Scenario)) +
   geom_line()
 timeline_
 
-# create NPV bar chart across scenarios [CURRENTLY BASED ON val_data; WHICH DOESN'T SUM UP GM AND val_wtp]
-scenario_bars <- ggplot(val_data, aes(x = Scenario, y = NPV)) +
-  geom_bar(stat = "identity")
+# create NPV bar chart across scenarios
+scenario_bars <- ggplot(val_data2, aes(x = Scenario, y = NPV)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 scenario_bars
